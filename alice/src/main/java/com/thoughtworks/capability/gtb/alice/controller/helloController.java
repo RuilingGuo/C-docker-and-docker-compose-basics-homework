b82d24deffd8a0b452834fe8c476856a4d4924f2
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-23 17:47:18 +0800

    finish task

--- alice/src/main/java/com/thoughtworks/capability/gtb/alice/controller/helloController.java
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/controller/helloController.java
@@ -1,4 +1,18 @@
 package com.thoughtworks.capability.gtb.alice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
 public class helloController {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return restTemplate.getForObject("http://bob:8081/hello", String.class);
+    }
+
 }
+
--- bob/src/main/java/com/thoughtworks/capability/gtb/bob/controller/helloController.java
+++ bob/src/main/java/com/thoughtworks/capability/gtb/bob/controller/helloController.java
@@ -1,4 +1,4 @@
-package com.thoughtworks.capability.gtb.alice.controller;
+package com.thoughtworks.capability.gtb.bob.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
